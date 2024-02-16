c565328301742d12635b30efd2d818ceb7049e30
ChinRuiqi <160222799+ChinRuiqi@users.noreply.github.com>
2024-02-16 17:52:10 +0800

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -15,22 +15,26 @@ st.title("Welocme to BMI calculator")
 weight = st.number_input("Enter your weight in KG", step = 0.1)
 
 height = st.number_input("Enter your height in Meters", step = 0.01)
-if weight > 650:
-    st. error("Weight out of range")
-        
+
+
 def calculate_bmi():
-    bmi = weight/(height)**2
-    bmi_thresholds = [18.5, 23, 27.5]
-    level_labels = ['Risk of nutritional deficiency','Low Risk','Moderate Risk','High Risk']
-    if bmi <= bmi_thresholds[0]:
-        level = level_labels[0]
-    elif bmi <= bmi_thresholds[1]:
-        level = level_labels[1]
-    elif bmi <= bmi_thresholds[2]:
-        level = level_labels[2]
+    if weight < 10 or weight > 650:
+        st. error("Unable to calculate, weight out of range")
+    elif height < 0.6 or height > 3:
+        st. error("Unable to calculate, height out of range")
     else:
-        level = level_labels[3]
-    st. success(f"Your BMI is {bmi}. You are at {level}")
+        bmi = weight/(height)**2
+        bmi_thresholds = [18.5, 23, 27.5]
+        level_labels = ['Risk of nutritional deficiency','Low Risk','Moderate Risk','High Risk']
+        if bmi <= bmi_thresholds[0]:
+            level = level_labels[0]
+        elif bmi <= bmi_thresholds[1]:
+            level = level_labels[1]
+        elif bmi <= bmi_thresholds[2]:
+            level = level_labels[2]
+        else:
+            level = level_labels[3]
+        st. success(f"Your BMI is {bmi}. You are at {level}")
 
 button = st.button("Calculate BMI")
 if button:
