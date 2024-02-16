d118850e3246a9118b0b79d7ea196dc3d62ef929
ChinRuiqi <160222799+ChinRuiqi@users.noreply.github.com>
2024-02-16 18:26:25 +0800

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -13,12 +13,12 @@ st.title("Welcome to BMI calculator")
 #Input
 
 weight = st.number_input("Enter your weight in KG", step = 0.1)
-
-height = st.number_input("Enter your height in Meters", step = 0.01)
 if weight < 10 or weight > 650:
     st. error("Unable to calculate, weight out of range")
     quit()
-    
+
+height = st.number_input("Enter your height in Meters", step = 0.01)
+
 if height < 0.6 or height > 3:
     st. error("Unable to calculate, height out of range")
     quit()
